c127b1f1544e514920f7bc00ec434108f9cd3c72
lbarberiscanoni <hllbck7@gmail.com>
2016-12-30 22:12:42 -0500

    initial commit

---
+++ linked_networking.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import sys
+import time
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+class LinkedIn():
+
+    def __init__(self, username, password):
+        self.browser = webdriver.Chrome()
+        self.username = username
+        self.password = password
+
+    def login(self):
+        self.browser.get("https://www.linkedin.com/uas/login")
+        emailInpt = self.browser.find_element_by_id("session_key-login")
+        emailInpt.send_keys(self.username)
+        passInpt = self.browser.find_element_by_id("session_password-login")
+        passInpt.send_keys(self.password)
+        passInpt.submit()
+
+    def scrollToBottom(self):
+        for i in range(0, 50):
+            self.browser.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+            time.sleep(1)
+
+    def network(self):
+        self.browser.get("https://www.linkedin.com/people/pymk?trk=nav_responsive_sub_nav_pymk")
+        self.scrollToBottom()
+        connectionList = self.browser.find_elements_by_css_selector(".card-wrapper .image-wrapper .picture a")
+        for card in connectionList:
+            url = card.get_attribute("href")
+            self.browser.execute_script("window.open('" + str(url) + "');window.scrollBy(0, document.body.scrollHeight);")
+            time.sleep(2)
+
+    def run(self):
+        self.login()
+        time.sleep(2)
+        self.network()
+
+networker = LinkedIn(username, password)
+networker.run()
+
